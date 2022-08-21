cbb9c08480f8934392684ea7513179552169bdc9
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-08-21 16:13:31 +0430

    test username and email --> class attribute

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,6 +4,9 @@ from django.contrib.auth import get_user_model
 
 
 class SignUpPageTests(TestCase):
+    username = 'myusername'
+    email = 'myusername@gmail.com'
+
     def test_signup_url_by_name(self):
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
@@ -14,11 +17,9 @@ class SignUpPageTests(TestCase):
 
     def test_signup_form(self):
         user = get_user_model().objects.create_user(
-            'myusername',
-            'myusername@gmail.com',
+            self.username,
+            self.email,
         )
         self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(get_user_model().objects.all()[0].username, 'myusername')
-        self.assertEqual(get_user_model().objects.all()[0].email, 'myusername@gmail.com')
-
-
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
